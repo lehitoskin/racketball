06db66aa214563e02f947fb2430cfbcb6b61e689
Lehi Toskin <lehi_toskin@lavabit.com>
2013-06-06 22:57:09 -0700

    Changed the way we call the c functions

--- lib/libgit2.rkt
+++ lib/libgit2.rkt
@@ -1,9 +1,14 @@
+; libgit2.rkt
 #lang racket
+(require ffi/unsafe
+	 ffi/unsafe/define)
 
-(require ffi/unsafe)
+(define-ffi-definer define-git (ffi-lib "libgit2_racket.so"))
 
-(define libgit2 (ffi-lib "libgit2_racket.so"))
+(define-git racket_git_clone (_fun _string _string -> _int))
+(define-git racket_git_branch_create (_fun _string _int -> _int))
 
-(define (git-clone url path)
-  ((get-ffi-obj "racket_git_clone" libgit2 (_fun _string _string -> _int))
-   url path))
+#|(racket_git_clone "https://github.com/lehitoskin/racketball.git"
+	   "/tmp/rball")|#
+
+(racket_git_branch_create "new-dev" 0)
