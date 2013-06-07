f69fdcec1375e75c8d38e194ee537fca9b2dca40
Lehi Toskin <lehi_toskin@lavabit.com>
2013-06-07 15:05:00 -0700

    target is messing with my head

--- lib/libgit2_racket.c
+++ lib/libgit2_racket.c
@@ -8,8 +8,8 @@ int racket_git_clone(char *url, char *path) {
 
 int racket_git_branch_create(char *branch_name, int force) {
 	git_reference *out = NULL;
-	git_repository *repo;
-	const git_commit *target;
+	git_repository *repo = NULL;
+	const git_commit *target = NULL;
 
 	return git_branch_create(&out, repo, branch_name, target, force);
 }
