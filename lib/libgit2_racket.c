7cd64fb1025cde0e5d4e0616e0b4747f97faa76c
Lehi Toskin <lehi_toskin@lavabit.com>
2013-06-06 19:18:24 -0700

    Indentation was bugging me

--- lib/libgit2_racket.c
+++ lib/libgit2_racket.c
@@ -1,7 +1,10 @@
 #include <git2.h>
 
 int racket_git_clone(char *url, char *path) {
-  git_repository *out = NULL;
+	git_repository *out = NULL;
 
 	return git_clone(&out, url, path, NULL);
+}
+
+int main() {
 }
