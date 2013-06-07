d7a934bd7578719efedf011988019aa3545b0c26
Lehi Toskin <lehi_toskin@lavabit.com>
2013-06-06 22:58:15 -0700

    Started on git_branch_create

--- lib/libgit2_racket.c
+++ lib/libgit2_racket.c
@@ -6,5 +6,13 @@ int racket_git_clone(char *url, char *path) {
 	return git_clone(&out, url, path, NULL);
 }
 
+int racket_git_branch_create(char *branch_name, int force) {
+	git_reference *out = NULL;
+	git_repository *repo;
+	const git_commit *target;
+
+	return git_branch_create(&out, repo, branch_name, target, force);
+}
+
 int main() {
 }
