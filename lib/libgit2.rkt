37249139d6fa82b91f2062f4722bff748cf98df2
Lehi Toskin <lehi_toskin@lavabit.com>
2013-06-14 20:03:49 -0700

    git_open needs to come before the other functions

--- lib/libgit2.rkt
+++ lib/libgit2.rkt
@@ -3,12 +3,22 @@
 (require ffi/unsafe
 	 ffi/unsafe/define)
 
+; using the libgit2 library
 (define-ffi-definer define-git (ffi-lib "libgit2_racket.so"))
 
+; function definitions
 (define-git racket_git_clone (_fun _string _string -> _int))
 (define-git racket_git_branch_create (_fun _string _int -> _int))
+(define-git racket_git_repository_open (_fun _string -> _int))
 
-#|(racket_git_clone "https://github.com/lehitoskin/racketball.git"
-	   "/tmp/rball")|#
+; variable definitions
+(define external-repo "https://github.com/lehitoskin/racketball.git")
+(define local-repo "/tmp/rball/")
+(define git-repo (string-append local-repo ".git"))
 
-(racket_git_branch_create "new-dev" 0)
+; procedure examples
+(racket_git_clone external-repo local-repo)
+
+(racket_git_repository_open git-repo)
+
+;(racket_git_branch_create "new-dev" 0)
--- lib/libgit2_racket.c
+++ lib/libgit2_racket.c
@@ -14,5 +14,12 @@ int racket_git_branch_create(char *branch_name, int force) {
 	return git_branch_create(&out, repo, branch_name, target, force);
 }
 
+// repo_name must end in .git
+int racket_git_repository_open(char *repo_name) {
+	git_repository *repo;
+
+	return git_repository_open(&repo, repo_name);
+}
+
 int main() {
 }
